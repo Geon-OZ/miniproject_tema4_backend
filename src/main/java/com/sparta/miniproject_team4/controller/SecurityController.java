2e52580edb3561a6076dc103638637add38fcf8a
Geon-OZ <tkfkadlekd10@gmail.com>
2022-06-15 16:58:59 +0900

    로그인한유저 닉네임 리턴값 json형식으로 수정

--- src/main/java/com/sparta/miniproject_team4/controller/SecurityController.java
+++ src/main/java/com/sparta/miniproject_team4/controller/SecurityController.java
@@ -1,5 +1,6 @@
 package com.sparta.miniproject_team4.controller;
 
+import com.sparta.miniproject_team4.dto.CurrentUserName;
 import com.sparta.miniproject_team4.security.UserDetailsImpl;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -12,12 +13,16 @@ public class SecurityController {
 
     @GetMapping("/api/nickname")
     @ResponseBody
-    public String currentNickName(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        if (userDetails != null) {
-            return userDetails.getNickname();
+    public CurrentUserName currentUserName(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        CurrentUserName currentUserName = new CurrentUserName();
+        if (userDetails != null){
+            currentUserName.setNickname(userDetails.getNickname());
+            return currentUserName;
         }else {
-        return null;
+            currentUserName.setNickname(null);
+            return currentUserName;
         }
+
     }
 
     @GetMapping("/api")
